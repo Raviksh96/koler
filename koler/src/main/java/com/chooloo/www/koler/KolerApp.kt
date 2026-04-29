e95f49356c709eb714b13d418d688e2fcd814355
Ravi Sh <83340804+Raviksh96@users.noreply.github.com>
2026-04-29 08:16:07 +0530

    Update KolerApp.kt

--- koler/src/main/java/com/chooloo/www/koler/KolerApp.kt
+++ koler/src/main/java/com/chooloo/www/koler/KolerApp.kt
@@ -7,6 +7,17 @@ import com.chooloo.www.chooloolib.notification.CallNotification
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
+import com.google.android.material.color.DynamicColors  // add this import
+
+@HiltAndroidApp
+class KolerApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)  // ← add this line
+        // ... rest of your existing code
+    }
+}
+
 @HiltAndroidApp
 open class KolerApp : ChoolooApp() {
     @Inject lateinit var telecomManager: TelecomManager
